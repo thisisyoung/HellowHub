3852bd4b8a8ce9bdb04bb8138795b22c98bb0823
Young <thisisyounger@gmail.com>
2012-09-05 15:31:27 +0800

    viewstate encryption testing

--- Site.master.cs
+++ Site.master.cs
@@ -9,6 +9,16 @@ public partial class SiteMaster : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (IsPostBack)
+        {
+            ViewState.Add("Young","IamYoung");
+        }
 
+        ViewState["Young"] = "IamYoungIamYoungIamYoungIamYoungIamYoungIamYoung";
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        //Response.Redirect("/Testpage1.aspx");
+        Server.Transfer("/Testpage1.aspx");
     }
 }
